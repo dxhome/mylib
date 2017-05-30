fae2350bec2a19a7555eab3d173903d73999a935
dxhome <dxhome@hotmail.com>
2017-05-30 21:48:15 +0800

    update createStreamTrimmer

--- .mylib.js
+++ .mylib.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const through = require('through');
 const mylib = require('storj-lib');
 const consts = require('./.consts');
 
@@ -27,11 +28,95 @@ const consts = require('./.consts');
 //     return true;
 // };
 
-// update file info
+/**
+ * update file hmac info
+ * @param {String} bucketid - build id
+ * @param {String} fileid - file id
+ * @param {Object} hmac - hmac info
+ * @param {String} hmac.type - hmac type, md5, sha256, ...
+ * @param {String} hmac.value - digest value
+ * @param callback
+ * @returns
+ */
 mylib.BridgeClient.prototype.updateFileInfo = function (bucketid, fileid, hmac, callback) {
     return this._request('PATCH', `/buckets/${bucketid}/files/${fileid}/info`, {
         hmac: hmac
     }, callback);
+};
+
+
+/**
+ * Returns the skip/limit params for downloading a file slice
+ * @private
+ * @param {Object} frame - The frame object from the bridge
+ * @param {Number} bytesStart - The starting byte for slice
+ * @param {Number} bytesEnd - The ending byte for slice
+ */
+mylib.BridgeClient.prototype._getSliceParams = function(frame, bytesStart, bytesEnd) {
+    var skip = 0;
+    var limit = 0;
+    var count = 0;
+    var trimFront = 0;
+    var trimBack = 0;
+    var trimFrontSet = false;
+    var trimBackSet = false;
+
+    frame.shards.forEach(function(shard) {
+        count += shard.size;
+
+        if (bytesStart > count) {
+            skip++;
+        } else if (!trimFrontSet) {
+            trimFront = bytesStart - ( count - shard.size );
+            trimFrontSet = true;
+        }
+
+        if (bytesEnd > count) {
+            limit++;
+        } else if (!trimBackSet){
+            trimBack = count - bytesEnd;
+            trimBackSet = true;
+        }
+    });
+
+    return {
+        skip: skip,
+        limit: limit,
+        trimFront: trimFront,
+        trimBack: trimBack
+    };
+};
+
+/**
+ * Returns a through stream that trims the output based on the given range
+ * @param {Number} trimFront - Number of bytes to trim off front of stream
+ * @param {Number} totalBytes - The total length of the stream in bytes
+ */
+mylib.utils.createStreamTrimmer = function (trimFront, totalBytes) {
+    let next = 0;
+    let readBytes = 0;
+
+    return through(function(data) {
+        next += data.length;
+        let offset = next - data.length;
+
+        if (readBytes >= totalBytes) {
+            return this.queue(null);
+        }
+
+        if (trimFront > next) {
+            return this.queue(new Buffer([]));
+        }
+
+        let rangeStart = (offset > trimFront) ? 0 : (trimFront - offset);
+        let rangeEnd = (next > (trimFront + totalBytes)) ?
+            (trimFront + totalBytes) - offset : data.length;
+
+        let trimmedSlice = data.slice(rangeStart, rangeEnd);
+        readBytes += trimmedSlice.length;
+
+        this.queue(trimmedSlice);
+    });
 };
 
 mylib.constants.NET_REENTRY = 30000;
